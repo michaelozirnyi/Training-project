87b4fc7b961a1f58556a86571e416f830e1e707a
Mykhailo <mykhailoozirnyi@gmail.com>
2017-02-01 16:49:36 +0200

    contunie Learning JavaScript, finished - Основы JavaScript

--- Изучение JS/js/myscript.js
+++ Изучение JS/js/myscript.js
@@ -148,19 +148,87 @@
 //     console.log( "номер " + i + "!" );
 // }
 
-
 // var num;
 // do {
 //     num = prompt("Введите число больше 100?", 0);
 // } while (num <= 100 && num != null);
 
+// if (browser == 'IE'){
+//     alert( 'О, да у вас IE!' );
+// } else  if(browser == 'Chrome'
+//     || browser == 'Firefox'
+//     || browser == 'Safari'
+//     || browser == 'Opera' ) {
+//     alert('Да, и эти браузеры мы поддерживаем');
+// } else {
+//     alert( 'Мы надеемся, что и в вашем браузере все ок!' );
+// }
+
+// var a = +prompt('a?', '');
+// switch (a){
+//     case 0:
+//         alert(0);
+//         break;
+//     case 1:
+//         alert(1);
+//         break;
+//     case 2:
+//     case 3:
+//         alert('2,3');
+//         break;
+// }
 
 
+// function checkAge(age) {
+//     if (age > 18) {
+//         return true;
+//     } else {
+//         return confirm('Родители разрешили?');
+//     }
+// }
+//
+// function checkAge(age) {
+//     return  (age > 18) ?  true : confirm('Родители разрешили?');
+// }
+//
+// function checkAge(age) {
+//     return (age > 18) || confirm('Родители разрешили?');
+// }
 
 
+// function min(a, b) {
+//     if (a < b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// }
+//
+// function min(a, b) {
+//     return a < b ? a : b;
+// }
 
 
+// var userName = 'Вася';
+//
+// function showMessage() {
+//     userName = 'Петя'; // (1) присвоение во внешнюю переменную
+//
+//     var message = 'Привет, я ' + userName;
+//      alert( message );
+// }
+//
+// showMessage();
+//
+// alert( userName ); // Петя, значение внешней переменной изменено функцией
 
+// function sum(a, b) {
+//     var result = a + b;
+//
+//     return result;
+// }
+// // использование:
+// alert( sum(25, 45) ); // 3
 
 
 
